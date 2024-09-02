19cd4ab69973c7f3212c6bf022559b7ed720415a
H3xadecimal <h3xadecimal@afternode.cn>
2024-09-02 21:47:09 +0800

    mod setup

---
+++ mod/common/src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "nullprotect";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
