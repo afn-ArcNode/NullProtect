e402a3e9c65b888118cf63a9547fe3768ea9dcc7
Zyklone <h3xadecimal@afternode.cn>
2024-09-07 19:47:44 +0800

    feat(server): silent database logger

--- server/common/src/main/kotlin/arcnode/nullprotect/server/DatabaseManager.kt
+++ server/common/src/main/kotlin/arcnode/nullprotect/server/DatabaseManager.kt
@@ -18,10 +18,15 @@ package arcnode.nullprotect.server
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import com.zaxxer.hikari.pool.HikariPool
 import kotlinx.coroutines.asCoroutineDispatcher
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.core.config.Configurator
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.exposedLogger
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.concurrent.ExecutorService
@@ -46,6 +51,7 @@ class DatabaseManager {
         conf.jdbcUrl = jdbcUrl
         conf.username = username
         conf.password = password
+        preSetup(conf)
 
         db = Database.connect(HikariDataSource(conf))
         this.setup()
@@ -56,9 +62,28 @@ class DatabaseManager {
 
         val conf = HikariConfig()
         conf.jdbcUrl = jdbcUrl
+        preSetup(conf)
 
         db = Database.connect(HikariDataSource(conf))
         this.setup()
+    }
+
+    private fun preSetup(conf: HikariConfig) {
+//        val unsafeField = Unsafe::class.java.getDeclaredField("theUnsafe")
+//        unsafeField.isAccessible = true
+//        val unsafe = unsafeField.get(null) as Unsafe
+//
+//        val cl = Class.forName("org.jetbrains.exposed.sql.SQLLogKt")
+//        val f = cl.getDeclaredField("exposedLogger")
+//        val off = unsafe.staticFieldOffset(f)
+//        unsafe.putObject(cl, off, NOPLogger.NOP_LOGGER)
+
+        conf.poolName = "NullProtect"
+
+        Configurator.setLevel(exposedLogger.name, Level.OFF)
+        Configurator.setLevel(HikariConfig::class.java.name, Level.OFF)
+        Configurator.setLevel(HikariDataSource::class.java.name, Level.OFF)
+        Configurator.setLevel(HikariPool::class.java.name, Level.OFF)
     }
 
     private fun setup() {
